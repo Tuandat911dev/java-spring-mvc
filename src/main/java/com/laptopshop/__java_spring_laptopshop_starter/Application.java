1864aaa56a03c5dec8983da5b120b0c5a0ac0503
PhamTuanDat <pdat100111@gmail.com>
2024-12-10 17:40:39 +0700

    lesson 1

---
+++ src/main/java/com/laptopshop/__java_spring_laptopshop_starter/Application.java
@@ -0,0 +1,13 @@
+package com.laptopshop.__java_spring_laptopshop_starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
